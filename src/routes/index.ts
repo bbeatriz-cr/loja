9f7061ad9aebba0c3fafc6b06581dd2c67dfba6a
BEATRIZ DA SILVA CRUZ <beatriz.scruz7@senacsp.edu.br>
2024-11-21 17:13:44 -0300

    CRUD

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,5 +9,11 @@ router.get('/',(req,res)=>{
 
 router.get('/produtos',produtoController.index)
 
+router.get('/cadastrar',produtoController.visualizarCadastro)
+router.post('/cadastrar',produtoController.cadastroProduto)
+
+router.get('/editar/:id',produtoController.editaProduto)
+router.post('/editar/:id',produtoController.atualizaProduto)
+
 
 export default router
